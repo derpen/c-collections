c3a40def1d58a7484dec1360ee8dbf886f92ad0f
derpen <rizqyassabil@hotmail.com>
2025-05-22 20:41:46 +0700

    Added some functions I guess idk lol

--- stopwatch-gui/src/main.c
+++ stopwatch-gui/src/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <raylib.h>
+#include <time.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 450
@@ -10,7 +11,16 @@
 #define TIME_ZONE_WIDTH (SCREEN_WIDTH / 2) - 35.0f
 #define TIME_ZONE_HEIGHT 100.0f
 
+#define MAX_HISTORY 64
+
+typedef struct {
+    double TotalTime;
+    time_t TimeStarted;
+    time_t TimeEnded;
+} TimeHistory;
+
 void draw_elapsed_time(double time, int32_t posX, int32_t posY);
+void draw_history(TimeHistory TimeList[], uint32_t TimeCounted);
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Stopwatch");
@@ -43,7 +53,6 @@ int main() {
         TIME_ZONE_HEIGHT,
     };
 
-
     Rectangle HistoryBox = {
         20.0f,
         (float)SCREEN_HEIGHT / 2,
@@ -52,6 +61,7 @@ int main() {
     };
 
     bool StopwatchStarted = false;
+    bool HasStartedOnce = false;
 
     uint32_t Fps = 0;
     SetTargetFPS(60);
@@ -62,10 +72,14 @@ int main() {
     double TimeElapsed = 0.0f;
     uint32_t TimeCounted = 0; // To handle how many items will be in history box
 
+    TimeHistory AllTime[MAX_HISTORY];
+
     while (!WindowShouldClose())
     {
         MousePoint = GetMousePosition();
-        TimeElapsed += GetFrameTime();
+
+        if (StopwatchStarted)
+            TimeElapsed += GetFrameTime();
 
         if (CheckCollisionPointRec(MousePoint, StartButton))
         {
@@ -73,20 +87,29 @@ int main() {
             {
                 StopwatchStarted = !StopwatchStarted;
 
-                // TODO: Handle date, write this to history box later
-                //time_t t = time(NULL);
-                //struct tm tm = *localtime(&t);
-                //printf("%d/%02d/%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+                // Started the clock
+                if (StopwatchStarted)
+                {
+                    AllTime[TimeCounted].TimeStarted = time(NULL);
+                }
 
+                // Stopped the clock
                 if (!StopwatchStarted)
                 {
+                    AllTime[TimeCounted].TimeEnded = time(NULL);
+                    AllTime[TimeCounted].TotalTime = TimeElapsed;
+
                     TotalTimeElapsed += TimeElapsed;
                     TimeElapsed = 0.0f;
                     TimeCounted += 1;
+
+                    if (!HasStartedOnce)
+                    {
+                        HasStartedOnce = true;
+                    }
                 }
             }
         }
-
 
         if (CheckCollisionPointRec(MousePoint, ResetButton))
         {
@@ -96,13 +119,15 @@ int main() {
                 TotalTimeElapsed = 0.0f;
                 TimeElapsed = 0.0f;
                 TimeCounted = 0;
+                HasStartedOnce = false;
             }
         }
+
         if (CheckCollisionPointRec(MousePoint, HistoryBox))
         {
             float MouseWheel = GetMouseWheelMove();
 
-            // Handle mouse scrolling in the history area here
+            // TODO: Handle mouse scrolling in the history area here
 
 
             //printf("Current Mouse Wheel: %f \n", MouseWheel);
@@ -110,6 +135,12 @@ int main() {
 
         BeginDrawing();
         ClearBackground(BLACK);
+
+        DrawRectangleRounded(TotalTimeBox, 0.1, 1, WHITE);
+        DrawRectangleRounded(CurrentTimeBox, 0.1, 1, WHITE);
+        DrawRectangleRounded(HistoryBox, 0.1, 1, WHITE);
+        draw_elapsed_time(TotalTimeElapsed, (int32_t) (SCREEN_WIDTH / 2) + MARGIN + 10 , (int32_t) MARGIN + 15);
+        draw_elapsed_time(TimeElapsed, (int32_t)MARGIN + 10, (int32_t)MARGIN + 15);
 
         if (StopwatchStarted) 
         {
@@ -120,7 +151,7 @@ int main() {
             DrawRectangleRounded(StartButton, 0.1, 1, GREEN);
         }
 
-        if (TimeCounted > 0)
+        if (HasStartedOnce)
         {
             DrawRectangleRounded(ResetButton, 0.1, 1, WHITE);
         }
@@ -128,13 +159,13 @@ int main() {
         {
             DrawRectangleRounded(ResetButton, 0.1, 1, GRAY);
         }
-        
-        DrawRectangleRounded(TotalTimeBox, 0.1, 1, WHITE);
-        DrawRectangleRounded(CurrentTimeBox, 0.1, 1, WHITE);
-        DrawRectangleRounded(HistoryBox, 0.1, 1, WHITE);
-        draw_elapsed_time(TotalTimeElapsed, (int32_t) (SCREEN_WIDTH / 2) + MARGIN + 10 , (int32_t) MARGIN + 15);
-        draw_elapsed_time(TimeElapsed, (int32_t)MARGIN + 10, (int32_t)MARGIN + 15);
 
+        if (HasStartedOnce)
+        {
+            //draw_history(AllTime, TimeCounted);
+	        DrawText("TIME ELAPSED\t\t\t\t\t\t\t\tDATE STARTED\t\t\t\t\t\t\t\tDATE ENDED", 50, (SCREEN_HEIGHT / 2) + 10, 20, RED);
+        }
+        
         EndDrawing();
     }
 
@@ -149,8 +180,25 @@ void draw_elapsed_time(double time, int32_t posX, int32_t posY)
 	int32_t TotalSecond = (int32_t) time % 60;
 	int32_t TotalMinute = (int32_t) (time / 60) % 60;
     int32_t TotalHour = (int32_t) time / 3600;
-	int8_t TotalTime[1000];
-	snprintf(TotalTime, 1000, "%s %02d:%02d:%02d \n", TimeText, TotalHour, TotalMinute, TotalSecond);
+	int8_t TotalTime[1024];
+	snprintf(TotalTime, 1024, "%s %02d:%02d:%02d \n", TimeText, TotalHour, TotalMinute, TotalSecond);
 	
 	DrawText(TotalTime, posX, posY, 20, RED);
 }
+
+void draw_history(TimeHistory TimeList[], uint32_t TimeCounted) 
+{
+    for (uint32_t i = 0; i < TimeCounted; i++)
+    {
+        printf("Time at : %d \n", i);
+
+	    //DrawText("TIME ELAPSED\t\t\t\t\t\t\t\tDATE STARTED\t\t\t\t\t\t\t\tDATE ENDED", 50, (SCREEN_HEIGHT / 2) + 10, 20, RED);
+
+        struct tm TimeStarted = *localtime(&TimeList[i].TimeStarted);
+	    printf("%d/%02d/%02d %02d:%02d:%02d\n", TimeStarted.tm_year + 1900, TimeStarted.tm_mon + 1, TimeStarted.tm_mday, TimeStarted.tm_hour, TimeStarted.tm_min, TimeStarted.tm_sec);
+
+        struct tm TimeEnded = *localtime(&TimeList[i].TimeEnded);
+	    printf("%d/%02d/%02d %02d:%02d:%02d\n", TimeEnded.tm_year + 1900, TimeEnded.tm_mon + 1, TimeEnded.tm_mday, TimeEnded.tm_hour, TimeEnded.tm_min, TimeEnded.tm_sec);
+    }
+}
+
